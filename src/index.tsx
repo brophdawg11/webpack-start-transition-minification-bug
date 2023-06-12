4c0decca1ab7ce5a9865837c3ffa0b9ff902d3b2
Matt Brophy <matt@brophy.org>
2023-06-12 16:17:46 -0400

    still Broken

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 const root = createRoot(document.getElementById('root')!);
+
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <LinkContainer to="/">
+                <span>foxCaves</span>
+            </LinkContainer>
+            &nbsp;
+            <NavLink to="/login">
+                <span>Login</span>
+            </NavLink>
+            &nbsp;
+            <NavLink to="/register">
+                <span>Register</span>
+            </NavLink>
+            <Routes>
+                <Route element={<h1>home</h1>} path="/" />
+                <Route element={<h1>Login</h1>} path="/login" />
+                <Route element={<h1>Register</h1>} path="/register" />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 root.render(
     <React.StrictMode>
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,29 +1,11 @@
-import { join } from 'node:path';
-// eslint-disable-next-line import/default
-import CopyPlugin from 'copy-webpack-plugin';
 // eslint-disable-next-line import/default
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-// eslint-disable-next-line import/default
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { Configuration, DefinePlugin } from 'webpack';
+import { Configuration } from 'webpack';
 import 'webpack-dev-server';
 
-// eslint-disable-next-line unicorn/prefer-module
-const PWD = __dirname;
-
-type NodeEnv = 'development' | 'production' | undefined;
-
-const mode: NodeEnv = (process.env.NODE_ENV as NodeEnv) ?? 'development';
-
 const config: Configuration = {
-    mode,
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     entry: './src/index.tsx',
-    output: {
-        path: join(PWD, 'build'),
-        publicPath: mode === 'development' ? '/' : '/', // '/static/',
-        filename: '[name].[contenthash].js',
-        chunkFilename: '[id].[contenthash].js',
-    },
     module: {
         rules: [
             {
@@ -31,39 +13,10 @@ const config: Configuration = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
         ],
     },
-    plugins: [
-        new DefinePlugin({
-            GIT_REVISION: JSON.stringify(process.env.GIT_REVISION),
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: 'public',
-                },
-            ],
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash].css',
-        }),
-    ],
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
     devServer: {
-        static: {
-            directory: join(PWD, 'public'),
-        },
         proxy: {
             '/api': {
                 target: 'https://foxcav.es:443',
